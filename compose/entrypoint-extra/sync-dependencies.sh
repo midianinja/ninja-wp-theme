a92678f0896f2df3371dbceece393e41aa9736e2
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-05-09 17:01:17 -0300

    remove todo o esquemático do composer para plugins

--- compose/entrypoint-extra/sync-dependencies.sh
+++ compose/entrypoint-extra/sync-dependencies.sh
@@ -4,24 +4,6 @@ WP_PLUGINS="/var/www/html/wp-content/plugins"
 WP_THEMES="/var/www/html/wp-content/themes"
 DEV_PLUGINS="/var/www/html/plugins"
 DEV_THEMES="/var/www/html/themes"
-VENDOR_PLUGINS="/var/www/html/vendor/plugins"
-VENDOR_THEMES="/var/www/html/vendor/themes"
-
-if [ -d "$VENDOR_PLUGINS" ];
-then
-    find "$VENDOR_PLUGINS" \
-        -maxdepth 1 \
-        -mindepth 1 \
-        -exec ln -sf {} $WP_PLUGINS \;
-fi
-
-if [ -d "$VENDOR_THEMES" ];
-then
-    find "$VENDOR_THEMES" \
-        -maxdepth 1 \
-        -mindepth 1 \
-        -exec ln -sf {} $WP_THEMES \;
-fi
 
 if [ -d "$DEV_PLUGINS" ];
 then

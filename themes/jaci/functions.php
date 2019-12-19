e20fcffa5e1b0ac1b157f72a94517995fdafd375
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-12-19 16:41:46 -0300

    exemplo de como adicionar novos estilos de título ao componente section-title

--- themes/jaci/functions.php
+++ themes/jaci/functions.php
@@ -20,3 +20,9 @@ add_filter( 'force_filtered_html_on_import' , '__return_false' );
 add_action( 'init',  function () {
 	remove_post_type_support( 'post', 'author' );
 });
+
+// filtro para adicionar mais classes de título
+add_filter('widgets\\SectionTitle:css_classes', function($css_classes){
+	$css_classes['title-jaci'] = __('Título Jaci', 'jaci');
+	return $css_classes;
+});

7fc8d2219a265a19f3a8c6ca06d9258d72869f10
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-12-19 16:23:59 -0300

    siteorigin pagebuilder

--- themes/jaci/functions.php
+++ themes/jaci/functions.php
@@ -2,13 +2,12 @@
 namespace jaci;
 
 define('THEME_DIR', __DIR__);
-define('WIDGETGROUP_MAIN', 'Jaci Widgets');
-define('WIDGETGROUP_SIDEBAR', 'Jaci Sidebar Widgets');
-
 require __DIR__ . '/library/settings.php';
 require __DIR__ . '/library/images.php';
 require __DIR__ . '/library/menus.php';
 require __DIR__ . '/library/assets.php';
+require __DIR__ . '/library/pagebuilder/index.php';
+
 // descomentar se for um multisite e for utilizar o plugin de sincronização de posts
 // require __DIR__ . '/library/mssync.php';
 
---
+++ themes/jaci/library/pagebuilder/index.php
@@ -0,0 +1,21 @@
+<?php
+define('WIDGETGROUP_MAIN', 'Jaci Widgets');
+define('WIDGETGROUP_SIDEBAR', 'Jaci Sidebar Widgets');
+
+/** Add SiteOrigin Page Builder custom fields class prefixes */
+add_filter('siteorigin_widgets_field_class_prefixes', function ($class_prefixes) {
+    $class_prefixes[] = 'Jaci_';
+    return $class_prefixes;
+});
+
+/** Add SiteOrigin Page Builder custom fields */
+add_filter('siteorigin_widgets_field_class_paths', function( $class_paths) {
+    $class_paths[] = get_template_directory() . '/library/pagebuilder-custom-fields/';
+    return $class_paths;
+});
+
+/** Add SiteOrigin Page Builder custom widgets */
+add_filter('siteorigin_widgets_widget_folders', function ($folders) {
+    $folders[] = get_template_directory() . '/library/pagebuilder-widgets/';
+    return $folders;
+});

d418c1be4f0b979f3eb639b51912afd958085263
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-12-19 16:48:03 -0300

    comentários no functions.php

--- themes/jaci/functions.php
+++ themes/jaci/functions.php
@@ -6,7 +6,6 @@ require __DIR__ . '/library/images.php';
 require __DIR__ . '/library/menus.php';
 require __DIR__ . '/library/assets.php';
 require __DIR__ . '/library/pagebuilder/index.php';
-
 // descomentar se for um multisite e for utilizar o plugin de sincronização de posts
 // require __DIR__ . '/library/mssync.php';
 
@@ -14,9 +13,13 @@ add_theme_support( 'align-wide' );
 add_theme_support( 'custom-logo' );
 add_theme_support( 'post-thumbnails' );
 
-add_filter( 'show_admin_bar', '__return_false' );
-add_filter( 'force_filtered_html_on_import' , '__return_false' );
+// descomentar para importar conteúdo
+// add_filter( 'force_filtered_html_on_import' , '__return_false' );
 
+// removemos a barra de admin para não cachear no cache de borda 
+add_filter( 'show_admin_bar', '__return_false' );
+
+// usamos uma taxonomia autor criada com o plugin Pods
 add_action( 'init',  function () {
 	remove_post_type_support( 'post', 'author' );
 });

5afe841408dfac702d58dfdb18363c92d905862d
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-06-02 17:22:38 -0300

    corrige caminhos dos componentes do pagebuild no tema Jaci

--- themes/jaci/library/pagebuilder/index.php
+++ themes/jaci/library/pagebuilder/index.php
@@ -10,12 +10,12 @@ add_filter('siteorigin_widgets_field_class_prefixes', function ($class_prefixes)
 
 /** Add SiteOrigin Page Builder custom fields */
 add_filter('siteorigin_widgets_field_class_paths', function( $class_paths) {
-    $class_paths[] = get_template_directory() . '/library/pagebuilder-custom-fields/';
+    $class_paths[] = get_template_directory() . '/library/pagebuilder/custom-fields/';
     return $class_paths;
 });
 
 /** Add SiteOrigin Page Builder custom widgets */
 add_filter('siteorigin_widgets_widget_folders', function ($folders) {
-    $folders[] = get_template_directory() . '/library/pagebuilder-widgets/';
+    $folders[] = get_template_directory() . '/library/pagebuilder/widgets/';
     return $folders;
 });

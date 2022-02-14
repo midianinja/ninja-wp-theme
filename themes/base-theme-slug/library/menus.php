1b8d4256a095bb2f8a816bd0a57a5f1c7f35570e
willow <brmagrini@gmail.com>
2022-02-13 22:40:10 -0300

    adiciona possibilidades de menus

--- themes/base-theme-slug/library/menus.php
+++ themes/base-theme-slug/library/menus.php
@@ -16,7 +16,7 @@ function custom_menus() {
 add_action('init', 'jaci\\custom_menus');
 
 
-
+// adiciona seta nos itens com subitens
 add_filter('walker_nav_menu_start_el', 'jaci\\add_arrow', 10, 4);
 function add_arrow($output, $item, $depth, $args) {
 
@@ -28,3 +28,25 @@ function add_arrow($output, $item, $depth, $args) {
     }
     return $output;
 }
+
+//Descomente para adicionar css para top menu (WIP)
+// add_filter('css_files_before_output','jaci\\add_top_menu', 10, 1);
+function add_top_menu($files){
+    $files['top-menu'] = array(
+        'file' => '_c-top-menu.css',
+        'global' => true,
+        'inline' => true,
+    );
+    return $files;
+}
+// Descomente para alterar menu para sempre funcionar como hamburguer
+// add_filter('css_files_before_output','jaci\\sempre_hamburguer', 10, 1);
+function sempre_hamburguer($files){
+    $files['sempre-hamburguer'] = array(
+        'file' => 'menu-sempre-hamburguer.css',
+        'global' => true,
+        'inline' => true,
+    );
+    return $files;
+}
+

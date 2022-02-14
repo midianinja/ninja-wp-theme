69850415b1db30ebce2541619f6d845d31a468a4
willow <brmagrini@gmail.com>
2022-02-13 22:39:58 -0300

    remove blocos especificos de projetos

--- themes/base-theme-slug/library/blocks/index.php
+++ themes/base-theme-slug/library/blocks/index.php
@@ -1,8 +1,5 @@
 <?php 
 
 require __DIR__ . '/featured-slider/index.php';
-require __DIR__ . '/estimatives-area/index.php';
-require __DIR__ . '/deforestation-info/index.php';
 require __DIR__ . '/embed-template/index.php';
 require __DIR__ . '/video-gallery/index.php';
-require __DIR__ . '/timeline/index.php';

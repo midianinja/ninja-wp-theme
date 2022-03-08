93e3bce3df7f1a00fedd187f8a7cda51c0c5e638
willow <brmagrini@gmail.com>
2022-03-08 14:21:05 -0300

    alterações no compilar e zip

--- dev-scripts/compilar.sh
+++ dev-scripts/compilar.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes/base-theme-slug
-docker run -it -v `pwd`:/compilar node:12 bash -c "cd compilar && npm install && npm run production && rm -rf node_modules"
+docker run -it -v `pwd`:/compilar node:12 bash -c "cd compilar && npm install && npm run production"

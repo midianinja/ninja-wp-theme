e2a7a74ab727595d8cf941d096686dd0bf80dd4e
willow <brmagrini@gmail.com>
2022-03-28 16:37:35 -0300

    remove pasta nao utilizada

--- compose/local/wordpress/wp-config.d/show-errors.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* PHP errors should be shown in a development environment */
-ini_set('display_errors','On');
-ini_set('error_reporting', E_ALL );
-define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', true);

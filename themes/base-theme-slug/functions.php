7f7f3564f6a26ec9d437171389c84e9a08cca3d6
willow <brmagrini@gmail.com>
2022-04-07 16:23:04 -0300

    atualiza hacklab-blocks e adiciona filtro para desabilitar blocos

--- themes/base-theme-slug/functions.php
+++ themes/base-theme-slug/functions.php
@@ -1,5 +1,12 @@
 <?php
-namespace jaci;
+namespace hacklabTema;
+
+// Blocos ativos:
+add_filter('hacklab_blocos_ativos','hacklabTema\\blocos_ativos');
+function blocos_ativos($blocos_ativos){
+    // unset($blocos_ativos['sample-block']);
+    return $blocos_ativos;
+}
 
 require __DIR__ . '/library/supports.php';
 require __DIR__ . '/library/sidebars.php';

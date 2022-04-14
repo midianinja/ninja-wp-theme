bdd18fee455a0e7265db1dd2002f092238a511d4
everaldomatias <matiaseveraldo@gmail.com>
2022-04-14 08:31:03 -0300

    Remove arquivo JS sem uso

--- themes/base-theme-slug/assets/javascript/functionalities/page.js
+++ themes/base-theme-slug/assets/javascript/functionalities/page.js
@@ -2,20 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var Slider = document.getElementById("main-slider");
 
-    Slider.querySelector("div.cwp_block_slider").onmouseover = function(e){
-        imgs = Slider.querySelectorAll("div.ultp-block-image");
-        imgs.forEach(el => {
-            el.style.transition = "opacity 0.3s linear 0s";
-            el.style.opacity = 0.7;
-        });
-    }
-
-    Slider.querySelector("div.cwp_block_slider").onmouseout = function(e){
-        imgs = Slider.querySelectorAll("div.ultp-block-image");
-        imgs.forEach(el => {
-            el.style.transition = "opacity 0.3s linear 0s";
-            el.style.opacity = 1;
-        });
+    if (Slider && Slider != 'null') {
+        Slider.querySelector("div.cwp_block_slider").onmouseover = function(e){
+            imgs = Slider.querySelectorAll("div.ultp-block-image");
+            imgs.forEach(el => {
+                el.style.transition = "opacity 0.3s linear 0s";
+                el.style.opacity = 0.7;
+            });
+        }
+    
+        Slider.querySelector("div.cwp_block_slider").onmouseout = function(e){
+            imgs = Slider.querySelectorAll("div.ultp-block-image");
+            imgs.forEach(el => {
+                el.style.transition = "opacity 0.3s linear 0s";
+                el.style.opacity = 1;
+            });
+        }
     }
 
 });

58118a3d81df7a368361f87b1c06ef1ab72e8f78
willow <brmagrini@gmail.com>
2022-05-03 15:15:02 -0300

    adiciona exemplo de interação com o bloco de posts via api

--- themes/base-theme-slug/functions.php
+++ themes/base-theme-slug/functions.php
@@ -1,6 +1,17 @@
 <?php
 namespace jaci;
 
+// exemplo de como adicionar elementos através dos filtros no bloco de posts via API
+// Descomentar a linha abaixo para adicionar, exemplo utilizando atributos (image_credits e meta_authors) adicionados à resposta da API no projeto de onde vem os posts.
+// add_filter('hacklab-fetch-posts-api-before-excerpt-embed_post','hacklabTema\\add_image_credits',10,3);
+function add_image_credits($content,$attributes, $post){
+    if($attributes['showImageCredit'] == true){
+        $content .= '<p class="post-image-credit">Foto: '.$post['image_credit'].'</p>';
+    }
+    $content .= '<p class="post-authors">'.$post['meta_authors'].'</p>';
+    return $content;
+}
+
 require __DIR__ . '/library/supports.php';
 require __DIR__ . '/library/sidebars.php';
 require __DIR__ . '/library/menus.php';

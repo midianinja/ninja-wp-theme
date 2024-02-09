8d319d3e48cf6ea41d216b0cd57d3e8dc62ec3ec
burns <burns@burnss-iMac.local>
2024-02-09 16:26:20 -0300

    ajustes de layout no header do site

--- themes/midia-ninja-theme/header.php
+++ themes/midia-ninja-theme/header.php
@@ -14,9 +14,10 @@
     /**
      * @link https://developer.wordpress.org/reference/hooks/wp_body_open/
      */
-    if ( function_exists( 'wp_body_open' ) )
+    if (function_exists('wp_body_open')) {
         wp_body_open();
-    ?>
+    }
+?>
     <header class="main-header">
         <div class="container">
             <div class="row">
@@ -25,22 +26,29 @@
 
                     <div class="logo">
                         <a href="<?= home_url() ?>">
-                            <img src="<?= get_template_directory_uri() ?>/assets/images/logo.png" width="200" alt="<?= get_bloginfo( 'name' ) ?>">
+                            <img src="<?= get_template_directory_uri() ?>/assets/images/logo.png" width="200" alt="<?= get_bloginfo('name') ?>">
                         </a>
                     </div>
+                    <button menu-container-class='primary-menu' class="toggle-menu" aria-label="<?= __("Toggle menu visibility", "base-textdomain") ?>">
 
-					<button menu-container-class='primary-menu' class="toggle-menu" aria-label="<?= __("Toggle menu visibility", "base-textdomain") ?>"> </button>
+                    </button>
+
                     <div class="menus">
-
-                        <div class="search-component">
-							<?php get_search_form(); ?>
-                        </div><!-- .search-component -->
+                        <?= wp_nav_menu(['theme_location' => 'main-menu', 'container' => 'nav', 'menu_id' => 'main-menu', 'menu_class' => 'menu', 'container_class' => 'primary-menu']) ?>
+                        
+                        <?php if (is_plugin_active('tutor/tutor.php')) : ?>
+                            <div class="tutor-header-profile-menu-items">
+                                <?php tutor_multi_column_dropdown(); ?>
+                            </div><!-- .tutor-header-profile-menu -->
+                        <?php endif; ?>
                     </div>
+
+                    <div class="search-component">
+                        <?php get_search_form(); ?>
+                    </div><!-- .search-component -->
                 </div>
             </div>
         </div>
-
-		<?php wp_nav_menu(['theme_location' => 'main-menu', 'container' => 'nav', 'menu_id' => 'main-menu', 'menu_class' => 'menu', 'container_class' => 'primary-menu']) ?>
 
 	</header>
 	<div id="app">

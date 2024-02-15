3593579a90af2a4b80c3e03b612402eaf538defd
burns <burns@burnss-iMac.local>
2024-02-15 17:25:42 -0300

    inserção de icone site por hacklab no footer

--- themes/midia-ninja-theme/footer.php
+++ themes/midia-ninja-theme/footer.php
@@ -2,22 +2,19 @@
 <?php wp_reset_postdata() ?>
 
 <footer class="main-footer">
-    <div class="container">
-        
+    <div class="container">   
         <div class="row">
-
             <div class="col-md-12 footer-widgets-area">
-                
-                
+                <?php dynamic_sidebar('footer_widgets') ?>
+                <div class="footer-logo">
+                    <img src="<?= get_template_directory_uri() ?>/assets/images/site-por-hacklab.png" alt="site por hacklab">
                 </div>
-                
             </div>
+                
         </div>
-    </footer>
-<?php dynamic_sidebar('footer_widgets') ?>
-<?php wp_footer() ?>
-
+    </div>
 </footer>
+
 <?php wp_footer() ?>
 
 </body>

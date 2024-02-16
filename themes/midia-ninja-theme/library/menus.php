d689827ff5eadb21e46d4bb22bf87692fc060881
burns <burns@burnss-iMac.local>
2024-02-16 14:33:21 -0300

    adicionando menu item Mais+

--- themes/midia-ninja-theme/library/menus.php
+++ themes/midia-ninja-theme/library/menus.php
@@ -2,7 +2,8 @@
 
 namespace jaci;
 
-function custom_menus() {
+function custom_menus()
+{
     register_nav_menu('main-menu', __('Menu Principal', 'base-textdomain'));
     /* register_nav_menu('footer-menu', __('Menu de Rodapé', 'base-textdomain')); */
 
@@ -18,7 +19,8 @@ add_action('init', 'jaci\\custom_menus');
 
 // adiciona seta nos itens com subitens
 add_filter('walker_nav_menu_start_el', 'jaci\\add_arrow', 10, 4);
-function add_arrow($output, $item, $depth, $args) {
+function add_arrow($output, $item, $depth, $args)
+{
 
     //Only add class to 'top level' items on the 'primary' menu.
     if ($depth === 0) {
@@ -31,7 +33,8 @@ function add_arrow($output, $item, $depth, $args) {
 
 //Descomente para adicionar css para top menu (WIP)
 // add_filter('css_files_before_output','jaci\\add_top_menu', 10, 1);
-function add_top_menu($files){
+function add_top_menu($files)
+{
     $files['top-menu'] = array(
         'file' => '_c-top-menu.css',
         'global' => true,
@@ -41,7 +44,8 @@ function add_top_menu($files){
 }
 // Descomente para alterar menu para sempre funcionar como hamburguer
 // add_filter('css_files_before_output','jaci\\sempre_hamburguer', 10, 1);
-function sempre_hamburguer($files){
+function sempre_hamburguer($files)
+{
     $files['sempre-hamburguer'] = array(
         'file' => 'menu-sempre-hamburguer.css',
         'global' => true,
@@ -50,3 +54,20 @@ function sempre_hamburguer($files){
     return $files;
 }
 
+
+//Adiciona item Mais + no main menu
+function add_more_itens_on_menu($items, $args)
+{
+    if ($args->theme_location != 'main-menu') {
+        return $items;
+    }
+
+    $link  = '<li class="menu-item nav-item mais">
+                <a href="/login">Mais +</a>
+            </li>';
+
+    $items .= $link;
+    return $items;
+}
+
+add_action('wp_nav_menu_items', 'jaci\\add_more_itens_on_menu', 10, 2);

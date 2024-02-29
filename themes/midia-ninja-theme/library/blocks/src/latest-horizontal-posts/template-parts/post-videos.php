1e428ab02647ace0e698747fa6bf14557e5c3e79
Everaldo Matias <matiaseveraldo@gmail.com>
2024-02-29 19:09:11 -0300

    Add option to use YouTube videos on block Latest Horizontal Posts #5 #32

---
+++ themes/midia-ninja-theme/library/blocks/src/latest-horizontal-posts/template-parts/post-videos.php
@@ -0,0 +1,8 @@
+<a href="<?php echo $args['video']['video_url']; ?>" target="_blank">
+    <div class="post videos">
+        <div class="post-thumbnail">
+            <div class="play"></div>
+            <img src="<?php echo $args['video']['thumbnail']; ?>" alt="<?php echo $args['video']['title']; ?>">
+        </div>
+    </div>
+</a>

2b321d91a3ce50bf6fd08e11437aeed0d56cc59b
burns <burns@burnss-iMac.local>
2024-03-05 18:52:11 -0300

    ajuste de layout em single

--- themes/midia-ninja-theme/single.php
+++ themes/midia-ninja-theme/single.php
@@ -1,25 +1,25 @@
 <?php
 /**
  * The template for displaying all single posts
- * 
+ *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  */
 
 gt_set_post_view();
-get_header(); 
+get_header();
 
-$category = get_the_terms( $post->ID, 'category' );
+$category = get_the_terms($post->ID, 'category');
 ?>
 
 <div class="container">
     <main class="content">
-        <?php while ( have_posts() ) : the_post(); ?>
+        <?php while (have_posts()) : the_post(); ?>
             <article class="post">
                 <header class="post-header">
                     <div>
                         <div class="info">
                             <span class="term-<?= $category[0]->slug; ?>">
-                                <?php the_category( ', ' ); ?>
+                                <?php the_category(' '); ?>
                             </span> 
                         </div>
 
@@ -32,15 +32,15 @@ $category = get_the_terms( $post->ID, 'category' );
 
                     <div class="content-author">
                         
-                        <?php echo get_avatar( get_the_author_meta( 'ID' ), 70 );?>
+                        <?php echo get_avatar(get_the_author_meta('ID'), 70);?>
 
                         <div class="author">
                             <?php the_author(); ?>
-                            <time class="date" datetime="<?php echo get_the_date( 'c' ); ?>"><?php the_date(); ?></time>
+                            <time class="date" datetime="<?php echo get_the_date('c'); ?>"><?php the_date(); ?></time>
                         </div>
                         
-                        <div class="page--share">
-                            <?php get_template_part('template-parts/share-links'); ?>
+                        <div class="page-share">
+                            <?php echo do_shortcode('[addtoany]'); ?>
                         </div>
                     </div>
                     
@@ -49,14 +49,17 @@ $category = get_the_terms( $post->ID, 'category' );
                 <section class="post-content">
                     <?php the_content(); ?>
 
-                    <div class="page--share">
-                        <?php get_template_part('template-parts/share-links'); ?>
+                    <div class="page-share">
+                        <?php echo do_shortcode('[addtoany]'); ?>
+                    </div>
+
+                    <div class="comments">
+
                     </div>
                 </section>
 
                 <footer class="post-footer">
-                   
-
+                    <div class="related-posts"></div>
                 </footer>
             </article>
         <?php endwhile; ?>

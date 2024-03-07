9a786153a7e7c767a64710737306cce86a68234a
Everaldo Matias <matiaseveraldo@gmail.com>
2024-03-07 19:03:40 -0300

    Add Columnists option on block Latest Horizontal Posts #41

--- themes/midia-ninja-theme/library/blocks/src/latest-horizontal-posts/includes/columnists.php
+++ themes/midia-ninja-theme/library/blocks/src/latest-horizontal-posts/includes/columnists.php
@@ -2,5 +2,35 @@
 
 namespace Ninja;
 
-function columnists_get_contents() {
+function columnists_get_contents( $block_id ) {
+    $cache_key = 'ninja_columnists_data_'  . $block_id;
+    $cached_data = get_transient( $cache_key );
+
+    if ( false !== $cached_data ) {
+        return $cached_data;
+    }
+
+    $data = [];
+
+    $args = [
+        'echo'                    => false,
+        'number'                  => 999,
+        'authors_with_posts_only' => true,
+        'orderby'                 => 'count'
+    ];
+
+    $authors = coauthors_get_users( $args );
+    shuffle( $authors );
+
+    if ( ! empty( $authors ) ) {
+        foreach ( array_slice( $authors, 0, 10 ) as $author ) {
+            $data[] = $author;
+        }
+    }
+
+    if ( ! empty( $data ) ) {
+        set_transient( $cache_key, $data, 3600 );
+    }
+
+    return $data;
 }
--- themes/midia-ninja-theme/library/blocks/src/latest-horizontal-posts/latest-horizontal-posts.php
+++ themes/midia-ninja-theme/library/blocks/src/latest-horizontal-posts/latest-horizontal-posts.php
@@ -5,6 +5,7 @@ namespace Ninja;
 function latest_horizontal_posts_callback( $attributes ) {
     $slides_to_show = $attributes['slidesToShow'] ?? 3;
 
+    $block_id         = esc_attr( $attributes['blockId'] );
     $block_model      = ( isset( $attributes['blockModel'] ) && ! empty( $attributes['blockModel'] ) ) ? esc_attr( $attributes['blockModel'] ) : 'specials';
     $content_position = ( isset( $attributes['contentPosition'] ) && ! empty( $attributes['contentPosition'] ) ) ? esc_attr( $attributes['contentPosition'] ) : 'left';
     $custom_class     = isset( $attributes['className'] ) ? sanitize_title( $attributes['className'] ) : '';
@@ -42,23 +43,15 @@ function latest_horizontal_posts_callback( $attributes ) {
         }
 
         $has_content = flickr_get_contents( $api_key, $flickr_by_type, $data_id );
-    } elseif ( $block_model == 'videos' ) {
-        // Vídeos
-        require_once  __DIR__ . '/includes/videos.php';
+    }
 
-        $api_key = get_option( 'youtube_key', false );
-        $playlist_id = ( isset( $attributes['playlistId'] ) && ! empty( $attributes['playlistId'] ) ) ? esc_attr( $attributes['playlistId'] ) : false;
-        $posts_to_show = intval( $attributes['postsToShow'] );
+    if ( $block_model == 'columnists' ) {
+        // Co Authors
+        require_once  __DIR__ . '/includes/columnists.php';
+        $has_content = columnists_get_contents( $block_id );
+    }
 
-        if ( ! $api_key || ! $playlist_id ) {
-            if ( is_admin() || defined( 'REST_REQUEST' ) && REST_REQUEST ) {
-                return '<h2>' . __( 'Check the API Key or playlist ID', 'ninja' ) . '</h2>';
-            }
-            return;
-        }
-
-        $has_content = videos_get_contents( $api_key, $playlist_id, $posts_to_show );
-    } else {
+    if ( $block_model == 'most-read' || $block_model == 'specials' ) {
         // Posts
         global $latest_horizontal_posts_ids;
 
@@ -87,6 +80,24 @@ function latest_horizontal_posts_callback( $attributes ) {
         }
 
         $has_content = $posts_query;
+    }
+
+    if ( $block_model == 'videos' ) {
+        // Vídeos
+        require_once  __DIR__ . '/includes/videos.php';
+
+        $api_key = get_option( 'youtube_key', false );
+        $playlist_id = ( isset( $attributes['playlistId'] ) && ! empty( $attributes['playlistId'] ) ) ? esc_attr( $attributes['playlistId'] ) : false;
+        $posts_to_show = intval( $attributes['postsToShow'] );
+
+        if ( ! $api_key || ! $playlist_id ) {
+            if ( is_admin() || defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+                return '<h2>' . __( 'Check the API Key or playlist ID', 'ninja' ) . '</h2>';
+            }
+            return;
+        }
+
+        $has_content = videos_get_contents( $api_key, $playlist_id, $posts_to_show );
     }
 
     if ( ! $has_content ) {
@@ -130,19 +141,23 @@ function latest_horizontal_posts_callback( $attributes ) {
             // List of the posts to mount slider
             echo '<div class="latest-horizontal-posts-block__slides">';
 
-                if ( ! is_a( $has_content, 'WP_Query' ) ) {
-                    if ( $block_model == 'videos' ) {
-                         // Videos
-                        foreach( $has_content as $video ) :
-                            get_template_part( 'library/blocks/src/latest-horizontal-posts/template-parts/post', $block_model, ['video' => $video, 'attributes' => $attributes] );
-                        endforeach;
-                    } else {
-                        // Flickr
-                        foreach( $has_content as $photo ) :
-                            get_template_part( 'library/blocks/src/latest-horizontal-posts/template-parts/post', $block_model, ['photo' => $photo, 'attributes' => $attributes] );
-                        endforeach;
+                if ( $block_model == 'collection' ) {
+                    // Flickr
+                    foreach( $has_content as $photo ) :
+                        get_template_part( 'library/blocks/src/latest-horizontal-posts/template-parts/post', $block_model, ['photo' => $photo, 'attributes' => $attributes] );
+                    endforeach;
+                }
+
+                if ( $block_model == 'columnists' ) {
+                    // Co Authors
+                    foreach ( $has_content as $author ) {
+                        echo "<div class='slide'>";
+                            get_template_part( 'library/blocks/src/latest-horizontal-posts/template-parts/co', 'author', ['author' => $author] );
+                        echo "</div>";
                     }
-                } else {
+                }
+
+                if ( $block_model == 'most-read' ) {
                     // Posts
                     while ( $has_content->have_posts() ) :
                         $has_content->the_post();
@@ -154,6 +169,27 @@ function latest_horizontal_posts_callback( $attributes ) {
                             get_template_part( 'library/blocks/src/latest-horizontal-posts/template-parts/post', $block_model, ['post' => $post, 'attributes' => $attributes] );
                         echo "</div>";
                     endwhile;
+                }
+
+                if ( $block_model == 'specials' ) {
+                    // Posts
+                    while ( $has_content->have_posts() ) :
+                        $has_content->the_post();
+                        global $post;
+
+                        $latest_horizontal_posts_ids[] = $post->ID;
+
+                        echo "<div class='slide'>";
+                            get_template_part( 'library/blocks/src/latest-horizontal-posts/template-parts/post', $block_model, ['post' => $post, 'attributes' => $attributes] );
+                        echo "</div>";
+                    endwhile;
+                }
+
+                if ( $block_model == 'videos' ) {
+                    // Youtube
+                    foreach( $has_content as $video ) :
+                        get_template_part( 'library/blocks/src/latest-horizontal-posts/template-parts/post', $block_model, ['video' => $video, 'attributes' => $attributes] );
+                    endforeach;
                 }
 
             echo '</div><!-- .latest-horizontal-posts-block__slides -->';

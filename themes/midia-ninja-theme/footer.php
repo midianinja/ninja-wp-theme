0213e205aaff162277398e732883d2000cb54553
burns <burns@burnss-iMac.local>
2024-03-08 11:51:47 -0300

    ajuste de layout em footer

--- themes/midia-ninja-theme/footer.php
+++ themes/midia-ninja-theme/footer.php
@@ -3,15 +3,28 @@
 
 <footer class="main-footer">
     <div class="container">   
-        <div class="row">
-            <div class="col-md-12 footer-widgets-area">
-                <?php dynamic_sidebar('footer_widgets') ?>
-                <div class="footer-logo">
-                    <img src="<?= get_template_directory_uri() ?>/assets/images/site-por-hacklab.png" alt="site por hacklab">
-                </div>
-            </div>
-                
+        <div class="footer-logo">
+            <img class="logo-olhos" src="<?= get_template_directory_uri() ?>/assets/images/logo-olhos.png" alt="logo ninja">
+            <img class="logo" src="<?= get_template_directory_uri() ?>/assets/images/logo-white.png" alt="logo ninja">
+            <?php the_social_networks_menu(); ?>
         </div>
+
+        <div class="menus">
+            <?= wp_nav_menu(['theme_location' => 'main-menu', 'container' => 'nav', 'menu_id' => 'main-menu', 'menu_class' => 'menu', 'container_class' => 'primary-menu']) ?>
+            
+            <?php if (is_plugin_active('tutor/tutor.php')) : ?>
+                <div class="tutor-header-profile-menu-items">
+                    <?php tutor_multi_column_dropdown(); ?>
+                </div><!-- .tutor-header-profile-menu -->
+            <?php endif; ?>
+        </div>
+        
+        <div class="footer-credit">
+            <div class="content">
+                <img src="<?= get_template_directory_uri() ?>/assets/images/site-por-hacklab.png" alt="site por hacklab">
+            </div>
+            
+        </div>   
     </div>
 </footer>
 

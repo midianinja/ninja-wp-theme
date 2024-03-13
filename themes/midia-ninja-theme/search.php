64c4fb62d140f24030f7a87069c588e3ac55d978
burns <burns@burnss-iMac.local>
2024-03-13 15:14:25 -0300

    ajuste em search.php

--- themes/midia-ninja-theme/search.php
+++ themes/midia-ninja-theme/search.php
@@ -1,24 +1,39 @@
 <?php
 get_header();
+
+global $wp_query;
+$total_results = $wp_query->found_posts;
+$singular = $wp_query->found_posts > 1 ? 'results' : 'result';
+
 ?>
 
 <div class="index-wrapper">
     <div class="container">
         <div class="row">
-
-            <?php get_template_part( 'template-parts/title/search' ); ?>
-
-            <main class="col-md-9">
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <?php get_template_part( 'template-parts/content/post' ); ?>
-                <?php endwhile; ?>
-
-                <?php get_template_part( 'template-parts/content/pagination' ); ?>
-            </main>
+            <div class="title">
+                <h1><?php _e('Search Results', 'base-textdomain'); ?></h1>
+                <p>(<?= $total_results . " $singular";?>)</p>
+            </div>
 
             <aside class="col-md-3">
-                <?php dynamic_sidebar( 'sidebar-search' ) ?>
+                <h4><?php _e('Results', 'base-textdomain'); ?></h4>
+                <p><?php _e('Click below on what types of results you would like for your search', 'base-textdomain'); ?></p>
+                <?php the_category()?>
             </aside>
+
+            <main class="col-md-9">
+                <div>
+                    <div class="search-component">
+                        <?php get_search_form();?>
+                    </div>
+                   
+                    <?php while (have_posts()) : the_post(); ?>
+                        <?php get_template_part('template-parts/content/post'); ?>
+                    <?php endwhile; ?>
+                </div>
+                
+                <?php get_template_part('template-parts/content/pagination'); ?>
+            </main>
 
         </div><!-- /.row -->
     </div><!-- /.container -->

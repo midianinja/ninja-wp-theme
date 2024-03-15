9e3046b13aba2c9a1310d2a6bb14e5e3a4d1e09f
burns <burns@burnss-iMac.local>
2024-03-15 17:24:44 -0300

    ajustes em p-archive-opiniao

--- themes/midia-ninja-theme/archive-opiniao.php
+++ themes/midia-ninja-theme/archive-opiniao.php
@@ -1,19 +1,23 @@
 <?php
 get_header();
+
+
+$latest_posts = get_posts(['numberposts' => 10]);
 ?>
 
 <div class="container">
-    <?php echo get_layout_header( 'opiniao' ); ?>
+    <?php echo get_layout_header('opiniao'); ?>
     <main class="content col-md-9">
         <div class="posts">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'template-parts/content/post' ); ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <?php get_template_part('template-parts/content/post-opiniao'); ?>
             <?php endwhile; ?>
         </div>
 
-        <?php get_template_part( 'template-parts/content/pagination' ); ?>
+        <?php get_template_part('template-parts/content/pagination'); ?>
     </main>
-    <?php echo get_layout_footer( 'opiniao' ); ?>
+    
+    <?php echo get_layout_footer('opiniao'); ?>
 </div>
 
 <?php get_footer(); ?>

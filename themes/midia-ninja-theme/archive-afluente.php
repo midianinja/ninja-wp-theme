6d3834ff579dcd1eace7d84be99e5ddf9655b406
dioliviers <dioliviers@gmail.com>
2024-03-22 12:40:41 -0300

    inserido o search  na archive afluente

--- themes/midia-ninja-theme/archive-afluente.php
+++ themes/midia-ninja-theme/archive-afluente.php
@@ -5,10 +5,22 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  */
 get_header();
+/* if (isset($attributes) && is_array($attributes)) {
+    $coluna_direita = Ninja\latest_vertical_posts_callback($attributes);
+    foreach ($attributes as $coluna) {
+        echo $coluna;
+    }
+} else {
+     echo "A variável \$attributes não está definida ou não é um array.";
+} */
 ?>
 
 <div class="container">
     <main class="content col-md-9">
+        <div class="search">
+            <h2>Buscar por: </h2>
+            <?php get_template_part( 'template-parts/search-form' ); ?>
+        </div>
         <div class="posts">
             <?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'template-parts/content/post' ); ?>

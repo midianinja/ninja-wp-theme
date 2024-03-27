c4ec113858ba940a8918f228a7d3037ccdf588c8
burns <burns@burnss-iMac.local>
2024-03-27 15:55:25 -0300

    ajuste de layout em pagina fotografia

--- themes/midia-ninja-theme/archive-galeria.php
+++ themes/midia-ninja-theme/archive-galeria.php
@@ -13,13 +13,14 @@ get_header();
     <?php echo get_layout_header('galeria'); ?>
         
         <div class="posts">
-            <?php while (have_posts()) : the_post(); ?>
-                <?php get_template_part('template-parts/content/post-galeria'); ?>
-            <?php endwhile; ?>
-
-            <div class="acervo">
-                <?php flickr_get_contents($api_key, $flickr_by_type, $data_id, $block_id);?>
+            <div class="seletas">
+                <h4><?php _e('Select', 'ninja');?></h4>
+            
+                <?php while (have_posts()) : the_post(); ?>
+                    <?php get_template_part('template-parts/content/post-galeria'); ?>
+                <?php endwhile; ?>
             </div>
+            
         </div>
         <?php echo get_layout_footer('galeria'); ?>
 

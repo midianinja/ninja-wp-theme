f5f205afe9a1e9c43a0bca8365d8208f0c57677b
burns <burns@burnss-iMac.local>
2024-04-03 19:49:30 -0300

    ajuste de layout em single

--- themes/midia-ninja-theme/assets/javascript/functionalities/menu.js
+++ themes/midia-ninja-theme/assets/javascript/functionalities/menu.js
@@ -120,29 +120,44 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 
     //Menu reduzido no scroll
-    window.addEventListener("scroll", debounce(function() {
+    window.addEventListener("scroll", function() {
         const scroll = window.scrollY || document.documentElement.scrollTop;
         const mainHeader = document.querySelector(".main-header");
         
         if (scroll >= 1) {
+
+            if (mainHeader.classList.contains('scrolado')){
+                return;
+            }
             mainHeader.classList.add("scrolado");
         } else {
             mainHeader.classList.remove("scrolado");
         }
-    }, 1), { passive: true });
+    }, 1, { passive: true });
     
+
+    // window.addEventListener("scroll", debounce(function() {
+    //     const scroll = window.scrollY || document.documentElement.scrollTop;
+    //     const mainHeader = document.querySelector(".main-header");
+        
+    //     if (scroll >= 1) {
+    //         mainHeader.classList.add("scrolado");
+    //     } else {
+    //         mainHeader.classList.remove("scrolado");
+    //     }
+    // }, 1), { passive: true });
     // Função de debounce para limitar a frequência de chamadas
-    function debounce(func, wait) {
-        let timeout;
-        return function executedFunction(...args) {
-            const later = () => {
-                clearTimeout(timeout);
-                func(...args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
-    }
+    // function debounce(func, wait) {
+    //     let timeout;
+    //     return function executedFunction(...args) {
+    //         const later = () => {
+    //             clearTimeout(timeout);
+    //             func(...args);
+    //         };
+    //         clearTimeout(timeout);
+    //         timeout = setTimeout(later, wait);
+    //     };
+    // }
     
 })
 

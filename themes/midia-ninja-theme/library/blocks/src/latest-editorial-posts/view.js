1b1893ef36e946d000552e9f110f1229f5cc46de
burns <burns@burnss-iMac.local>
2024-04-19 12:32:36 -0300

    ajustes de layout na home

--- themes/midia-ninja-theme/library/blocks/src/latest-editorial-posts/view.js
+++ themes/midia-ninja-theme/library/blocks/src/latest-editorial-posts/view.js
@@ -1,59 +1,71 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const filtersWrap = document.querySelectorAll('.latest-editorial-posts-block__filter')
-    const filters = document.querySelectorAll('.latest-editorial-posts-block__filter [data-term-id]')
+document.addEventListener("DOMContentLoaded", function () {
+	const filtersWrap = document.querySelectorAll(
+		".latest-editorial-posts-block__filter",
+	);
+	const filters = document.querySelectorAll(
+		".latest-editorial-posts-block__filter [data-term-id]",
+	);
 
-    filters.forEach(filter => {
-        filter.addEventListener('click', function() {
-            filters.forEach(filter => filter.classList.remove('active'))
-            filtersWrap.forEach(filter => filter.classList.remove('active'))
-            this.classList.add('active')
-            this.closest('.latest-editorial-posts-block__filter').classList.add('active')
-            const termId = this.getAttribute('data-term-id')
-            fetchPostsByCategoryId(termId)
-        })
-    })
+	filters.forEach((filter) => {
+		filter.addEventListener("click", function () {
+			filters.forEach((filter) => filter.classList.remove("active"));
+			filtersWrap.forEach((filter) => filter.classList.remove("active"));
+			this.classList.add("active");
+			this.closest(".latest-editorial-posts-block__filter").classList.add(
+				"active",
+			);
+			const termId = this.getAttribute("data-term-id");
+			fetchPostsByCategoryId(termId);
+		});
+	});
 
-    function fetchPostsByCategoryId(categoryId) {
-        const postsDiv = document.querySelector('.latest-editorial-posts-block__posts')
-        postsDiv.innerHTML = '<div class="loading">Loading...</div>'
+	function fetchPostsByCategoryId(categoryId) {
+		const postsDiv = document.querySelector(
+			".latest-editorial-posts-block__posts",
+		);
+		postsDiv.innerHTML = '<div class="loading">Loading...</div>';
 
-        let url = '/wp-json/wp/v2/posts?_embed&per_page=9'
-        if (categoryId) {
-            url = `/wp-json/wp/v2/posts?categories=${categoryId}&_embed&per_page=9`
-        }
+		let url = "/wp-json/wp/v2/posts?_embed&per_page=9";
+		if (categoryId) {
+			url = `/wp-json/wp/v2/posts?categories=${categoryId}&_embed&per_page=9`;
+		}
 
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok')
-                }
-                return response.json()
-            })
-            .then(posts => {
-                if (posts.length > 0) {
-                    updatePostsDiv(posts)
-                } else {
-                    postsDiv.innerHTML = '<div class="not-found">Not Found</div>'
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching posts:', error)
-                postsDiv.innerHTML = '<div class="error">Error loading posts.</div>'
-            })
-    }
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				return response.json();
+			})
+			.then((posts) => {
+				if (posts.length > 0) {
+					updatePostsDiv(posts);
+				} else {
+					postsDiv.innerHTML = '<div class="not-found">Not Found</div>';
+				}
+			})
+			.catch((error) => {
+				console.error("Error fetching posts:", error);
+				postsDiv.innerHTML = '<div class="error">Error loading posts.</div>';
+			});
+	}
 
-    function updatePostsDiv(posts) {
-        const postsDiv = document.querySelector('.latest-editorial-posts-block__posts')
-        postsDiv.innerHTML = ''
+	function updatePostsDiv(posts) {
+		const postsDiv = document.querySelector(
+			".latest-editorial-posts-block__posts",
+		);
+		postsDiv.innerHTML = "";
 
-        posts.forEach(post => {
-            const postElement = document.createElement('a')
-            postElement.href = post.link
-            const imageUrl = post._embedded['wp:featuredmedia'] ? post._embedded['wp:featuredmedia'][0].source_url : 'https://via.placeholder.com/400'
+		posts.forEach((post) => {
+			const postElement = document.createElement("a");
+			postElement.href = post.link;
+			const imageUrl = post._embedded["wp:featuredmedia"]
+				? post._embedded["wp:featuredmedia"][0].source_url
+				: "https://via.placeholder.com/400";
 
-            let termsHtml = '';
-            if (post.rendered_categories[0] !== undefined) {
-                termsHtml = `
+			let termsHtml = "";
+			if (post.rendered_categories[0] !== undefined) {
+				termsHtml = `
                 <div class="post-thumbnail">
                     <span class="post--terms">
                         <ul class="list-terms tax-category">
@@ -62,35 +74,43 @@ document.addEventListener('DOMContentLoaded', function() {
                     </span>
                     <img src="${imageUrl}" alt="${post.title.rendered}">
                 </div>
-                `
-            } else {
-                termsHtml = `
+                `;
+			} else {
+				termsHtml = `
                 <div class="post-thumbnail">
                     <img src="${imageUrl}" alt="${post.title.rendered}">
                 </div>
-                `
-            }
+                `;
+			}
 
-            postElement.innerHTML = `
+			postElement.innerHTML = `
                 <div class="post">
                     ${termsHtml}
                     <div class="post-content">
                         <h2 class="post-title">${post.title.rendered}</h2>
                         <div class="post-meta">
-                            <span class="post-meta--date">${new Date(post.date).toLocaleDateString()}</span>
+                            <span class="post-meta--date">${new Date(
+															post.date,
+														).toLocaleDateString()}</span>
                         </div>
                     </div>
                 </div>
-            `
-            postsDiv.appendChild(postElement)
-        })
-    }
+            `;
+			postsDiv.appendChild(postElement);
+		});
+	}
 
-    document.querySelectorAll('.clear-filter').forEach(item => {
-        item.addEventListener('click', function() {
-            document.querySelectorAll('.latest-editorial-posts-block__filter [data-term-id]').forEach(filter => filter.classList.remove('active'))
-            document.querySelectorAll('.latest-editorial-posts-block__filter').forEach(filter => filter.classList.remove('active'))
-            fetchPostsByCategoryId('')
-        })
-    })
-})
+	document.querySelectorAll(".clear-filter").forEach((item) => {
+		item.addEventListener("click", function () {
+			document
+				.querySelectorAll(
+					".latest-editorial-posts-block__filter [data-term-id]",
+				)
+				.forEach((filter) => filter.classList.remove("active"));
+			document
+				.querySelectorAll(".latest-editorial-posts-block__filter")
+				.forEach((filter) => filter.classList.remove("active"));
+			fetchPostsByCategoryId("");
+		});
+	});
+});

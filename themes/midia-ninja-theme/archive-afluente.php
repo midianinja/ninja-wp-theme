2e3877ca9688154b1a495d41b08067b146147bac
dioliviers <dioliviers@gmail.com>
2024-04-23 17:46:58 -0300

    ajuste fino no banner de afluentes

--- themes/midia-ninja-theme/archive-afluente.php
+++ themes/midia-ninja-theme/archive-afluente.php
@@ -27,10 +27,7 @@ get_header();
             <?php while (have_posts()) : the_post(); ?>
                 <?php get_template_part('template-parts/content/post'); ?>
             <?php endwhile; ?>
-        </div>
-
-        <?php get_template_part('template-parts/content/pagination'); ?>
-        
+        </div>        
     </main>
 
     <?php echo get_layout_footer('afluentes'); ?>

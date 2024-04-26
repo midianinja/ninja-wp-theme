ec4bf8fb660822b8b6ee1e5a558d49b81d77cb19
dioliviers <dioliviers@gmail.com>
2024-04-26 13:48:52 -0300

    ajustes na archive de afluentes

--- themes/midia-ninja-theme/template-parts/search-afluente.php
+++ themes/midia-ninja-theme/template-parts/search-afluente.php
@@ -1,8 +1,9 @@
-
-<form action="" class="advanced-search">
-    <h2>Buscar por: </h2>
+<form action="" method="GET" class="advanced-search">
+    <h2>Buscar por:</h2>
     <div class="input-search">
-        <input type="text" name="pesquisar" value="<?= isset($_GET['pesquisar']) ? $_GET['pesquisar'] : '' ?>">
-        <i class="fas fa-search"></i>
+        <input type="text" placeholder="Afluentes..." name="pesquisar" value="<?= isset($_GET['pesquisar']) ? $_GET['pesquisar'] : '' ?>">
+        <button type="submit">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/generic.png'; ?>" alt="Search Icon">
+        </button>
     </div>
 </form>

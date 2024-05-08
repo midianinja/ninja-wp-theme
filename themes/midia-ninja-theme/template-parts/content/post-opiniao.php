dd51aa8b445c10430d644a4e7c5e2cfba115bfbc
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2024-05-08 13:45:57 -0300

    corrige  avatares dos colunistas na pagina de busca

--- themes/midia-ninja-theme/template-parts/content/post-opiniao.php
+++ themes/midia-ninja-theme/template-parts/content/post-opiniao.php
@@ -1,14 +1,42 @@
+<?php
+
+$coauthors = get_coauthors();
+$get_coauthors = [];
+
+$has_columnist = false;
+
+foreach( $coauthors as $coauthor ):
+    $coauthor_data = array();
+
+    $coauthor_data['author_id'] = '';
+    $coauthor_data['colunista'] = '';
+    if (is_a($coauthor, 'WP_User')) {
+        $coauthor_data['author_id'] = $coauthor->data->ID;
+        $coauthor_data['author_name'] = $coauthor->data->display_name;
+        $coauthor_data['userdata'] = get_userdata( $coauthor_data['author_id'] );
+    } else {
+        $coauthor_data['author_id'] = $coauthor->ID;
+        $coauthor_data['colunista'] = get_post_meta($coauthor_data['author_id'], 'colunista', true);
+        if( !$has_columnist && $coauthor_data['colunista'] == '1' ){
+            $has_columnist = true;
+        }
+    }
+    $get_coauthors[] = $coauthor_data;
+endforeach;
+
+
+?>
 <a class="post-card post-card-opiniao" href="<?php the_permalink(); ?>">
 
     <div class="post-card--thumb post-opiniao">
 
-
-        <?php if (has_post_thumbnail()) : ?>
+		<?php if ($get_coauthors[0]['colunista']): ?>
+			<?php echo get_avatar($get_coauthors[0]['author_id'], 70);?>
+        <?php elseif (has_post_thumbnail()) : ?>
             <?php the_post_thumbnail('medium'); ?>
         <?php else : ?>
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/default-image.png" alt="<?php the_title(); ?>">
         <?php endif; ?>
-
 
     </div><!-- /.post-card--thumb -->
 

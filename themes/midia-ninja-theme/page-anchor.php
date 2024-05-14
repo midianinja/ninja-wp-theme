0426bf470f7b316e5cb651de5f1f2d0844b0011d
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2024-05-14 17:03:15 -0300

    adiciona documentacao no template de anchoras

--- themes/midia-ninja-theme/page-anchor.php
+++ themes/midia-ninja-theme/page-anchor.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Página com âncoras
  */
-get_header(); 
+get_header();
 
 $post_id  = get_the_ID();
 $subtitle = get_post_meta($post_id, 'subtitulo', true);
@@ -12,7 +12,14 @@ $titulo_tag = get_post_meta($post_id, 'titulo_tag', true);
 
 <div class="index-wrapper page-with-title">
     <div class="container">
-    <?php echo get_layout_header( 'perguntas-frequentes' ); ?>
+		<?php
+
+			if ( is_page('perguntas-frequentes') ) :
+				echo get_layout_header( 'perguntas-frequentes' );
+			elseif( is_page('documentacao') ) :
+				echo get_layout_header( 'documentacao' );
+			endif;
+		?>
         <div class="row">
             <div class="col-md-3 sidebar-page">
                 <p class="anchor-title"><?= __('What is Mídia Ninja', 'ninja') ?></p>
@@ -22,7 +29,14 @@ $titulo_tag = get_post_meta($post_id, 'titulo_tag', true);
                 <?php the_content() ?>
             </div>
         </div>
-        <?php echo get_layout_footer( 'perguntas-frequentes' ); ?>
+        <?php
+
+			if ( is_page('perguntas-frequentes') ) :
+				echo get_layout_footer( 'perguntas-frequentes' );
+			elseif( is_page('documentacao') ) :
+				echo get_layout_footer( 'documentacao' );
+			endif;
+		?>
     </div>
 </div>
 

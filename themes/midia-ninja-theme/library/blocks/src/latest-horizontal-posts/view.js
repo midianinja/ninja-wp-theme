47b8dad5f88345ef405e86c35d1070b99168f847
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2024-09-09 11:32:21 -0300

    otimiza view.js dos blocos vertical e gorizontal

--- themes/midia-ninja-theme/library/blocks/src/latest-horizontal-posts/view.js
+++ themes/midia-ninja-theme/library/blocks/src/latest-horizontal-posts/view.js
@@ -3,39 +3,44 @@ import slick from 'slick-carousel'
 document.addEventListener("DOMContentLoaded", function() {
     const horizontalSliders = document.querySelectorAll('[data-slider="horizontal-posts"]')
 
-    horizontalSliders.forEach(slider => {
-        const slides = slider.querySelector('.latest-horizontal-posts-block__slides')
-        const arrows = slider.querySelector('.latest-horizontal-posts-block__arrows')
-        const dots = slider.querySelector('.latest-horizontal-posts-block__dots')
-        const slidesToShow = slider.dataset.slidesToShow || 3
+	if (horizontalSliders.length > 0) {
 
-        // Mobile
-        const arrowsMobile = slider.querySelector('.medium-only .latest-horizontal-posts-block__arrows')
-        const dotsMobile = slider.querySelector('.medium-only .latest-horizontal-posts-block__dots')
+		const { default: slick } = import('slick-carousel');
 
-		let slidesToShowMobile = 1
-		if (slider.classList.contains('model-specials') || slider.classList.contains('model-most-read')) {
-			slidesToShowMobile = 2
-		} else if (slider.classList.contains('model-collection') || slider.classList.contains('model-albums')) {
-			slidesToShowMobile = 1.5
-		}
+        horizontalSliders.forEach(slider => {
+			const slides = slider.querySelector('.latest-horizontal-posts-block__slides')
+			const arrows = slider.querySelector('.latest-horizontal-posts-block__arrows')
+			const dots = slider.querySelector('.latest-horizontal-posts-block__dots')
+			const slidesToShow = slider.dataset.slidesToShow || 3
 
-        jQuery(slides).slick({
-            appendArrows: arrows,
-            appendDots: dots,
-            dots: true,
-            infinite: false,
-            slidesToShow: parseInt(slidesToShow),
-            responsive: [
-                {
-                    breakpoint: 783,
-                    settings: {
-                        appendArrows: arrowsMobile,
-                        appendDots: dotsMobile,
-                        slidesToShow: slidesToShowMobile,
-                    }
-                }
-            ]
-        })
-    })
+			// Mobile
+			const arrowsMobile = slider.querySelector('.medium-only .latest-horizontal-posts-block__arrows')
+			const dotsMobile = slider.querySelector('.medium-only .latest-horizontal-posts-block__dots')
+
+			let slidesToShowMobile = 1
+			if (slider.classList.contains('model-specials') || slider.classList.contains('model-most-read')) {
+				slidesToShowMobile = 2
+			} else if (slider.classList.contains('model-collection') || slider.classList.contains('model-albums')) {
+				slidesToShowMobile = 1.5
+			}
+
+			jQuery(slides).slick({
+				appendArrows: arrows,
+				appendDots: dots,
+				dots: true,
+				infinite: false,
+				slidesToShow: parseInt(slidesToShow),
+				responsive: [
+					{
+						breakpoint: 783,
+						settings: {
+							appendArrows: arrowsMobile,
+							appendDots: dotsMobile,
+							slidesToShow: slidesToShowMobile,
+						}
+					}
+				]
+			})
+		})
+    }
 })
--- themes/midia-ninja-theme/library/blocks/src/latest-vertical-posts/view.js
+++ themes/midia-ninja-theme/library/blocks/src/latest-vertical-posts/view.js
@@ -3,17 +3,21 @@ import slick from 'slick-carousel'
 document.addEventListener("DOMContentLoaded", function() {
     const sliders = document.querySelectorAll('[data-slider="vertical-posts"]')
 
-    sliders.forEach(slider => {
-        const slides = slider.querySelector('.latest-vertical-posts-block__slides')
-        const arrows = slider.querySelector('.latest-vertical-posts-block__arrows')
-        const dots = slider.querySelector('.latest-vertical-posts-block__dots')
+	const { default: slick } = import('slick-carousel');
 
-        jQuery(slides).slick({
-            appendArrows: arrows,
-            appendDots: dots,
-            dots: true,
-            infinite: false,
-            slidesToShow: 1
-        })
-    })
+	if (sliders.length > 0) {
+		sliders.forEach(slider => {
+			const slides = slider.querySelector('.latest-vertical-posts-block__slides')
+			const arrows = slider.querySelector('.latest-vertical-posts-block__arrows')
+			const dots = slider.querySelector('.latest-vertical-posts-block__dots')
+
+			jQuery(slides).slick({
+				appendArrows: arrows,
+				appendDots: dots,
+				dots: true,
+				infinite: false,
+				slidesToShow: 1
+			})
+		})
+	}
 })

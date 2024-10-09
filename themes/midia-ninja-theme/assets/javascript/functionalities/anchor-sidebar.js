32e0cf1ab6ec02767dcd3ee8d63a1b062afdf208
burns <bfernandes.sp@gmail.com>
2024-10-09 17:12:55 -0300

    ajustes de layout em termos e condiçoes

--- themes/midia-ninja-theme/assets/javascript/functionalities/anchor-sidebar.js
+++ themes/midia-ninja-theme/assets/javascript/functionalities/anchor-sidebar.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let linkList = ''
     let anchors = document.getElementById("anchors")
-    let titles = document.querySelectorAll(".content > h1, .content > h2, .content > h3, .content > h4, .content > h5, .content > h6")
+    let titles = document.querySelectorAll(".content h1, .content h2, .content h3, .content h4, .content h5, .content h6")
 
     if (anchors && titles) {
         titles.forEach((title, index) => {
             let content = title.innerText.replace(/<[^>]+>/g, '')
             let id = `${index}-section`
-    
+
             title.setAttribute("id", `${id}`)
-    
+
             linkList += `<li><a href="#${id}">${content}</a></li>`
-    
+
         });
-    
+
         anchors.innerHTML = linkList
     }
 

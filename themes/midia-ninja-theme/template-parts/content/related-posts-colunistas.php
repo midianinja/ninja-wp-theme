4a474f70a32bc32901895eba28aa8bbd00d436c4
Thayná Zacharias <thayna.zacharias@hacklab.com.br>
2024-11-11 13:17:45 -0300

    correção em atualização de posts em rodapé de single de opinião para evitar repetição

--- themes/midia-ninja-theme/template-parts/content/related-posts-colunistas.php
+++ themes/midia-ninja-theme/template-parts/content/related-posts-colunistas.php
@@ -25,24 +25,24 @@ $limit = 3; // Quantidade de posts desejada
             $args['tax_query'] = [
                 [
                     'taxonomy' => 'author', // Ajuste para o termo correto se o Co-Authors Plus usa outra taxonomia
-                    'field'    => 'slug',
+                    'field'    => 'term_id',
                     'terms'    => $displayed_authors,
                     'operator' => 'NOT IN',
                 ],
             ];
         }
 
-        $query = get_posts($args);
+		$query = get_posts($args);
 
         // Se encontrar um post, adiciona aos resultados e armazena o autor
         if ($query){
 			setup_postdata($query[0]);
             // Obtém os co-autores do post
-            $coauthors = get_coauthors();
+			$coauthors_terms = get_the_terms( $query[0], 'author' );
 
             // Armazena os slugs dos co-autores exibidos
-            foreach ($coauthors as $coauthor) {
-                $displayed_authors[] = $coauthor->user_nicename;
+            foreach ($coauthors_terms as $coauthor) {
+                $displayed_authors[] = $coauthor->term_id;
             }
 
             // Armazena o post no array para exibição

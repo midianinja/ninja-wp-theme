dfb9b94bf90c29ecc18ef1a6d95b24072e29adbd
Filipe de Souza <filipe.souza@hacklab.com.br>
2025-11-10 13:22:15 -0300

    adiciona estilos para a archive de termos Ref.: no issue

--- themes/midia-ninja-theme/archive.php
+++ themes/midia-ninja-theme/archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -8,20 +9,43 @@
 get_header();
 ?>
 
-<div class="container">
-    <main class="content col-md-9">
-        <div class="posts">
-            <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'template-parts/content/post' ); ?>
-            <?php endwhile; ?>
-        </div>
+<div class="index-wrapper">
+	<div class="container">
+		<div class="row">
 
-        <?php get_template_part( 'template-parts/content/pagination' ); ?>
-    </main>
+			<div class="infos">
+				<div class="info">
+					<?php if ($main_category): ?>
+						<span class="category-<?= $main_category->slug ?>">
+							<a href="<?= get_category_link($main_category->term_id) ?>"><?= $main_category->name ?></a>
+						</span>
+					<?php endif; ?>
+					<?php foreach ($categories as $category): ?>
+						<?php if ($category->term_id !== $main_category->term_id): ?>
+							<span class="category-<?= $category->slug ?>">
+								<a href="<?= get_category_link($category->term_id) ?>"><?= $category->name ?></a>
+							</span>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				</div>
+			</div><!-- .infos -->
 
-    <aside class="col-md-3">
-        <?php dynamic_sidebar( 'sidebar-default' ) ?>
-    </aside>
+			<main class="content col-md-9">
+				<div class="posts">
+
+					<?php while (have_posts()) : the_post(); ?>
+						<?php get_template_part('template-parts/content/post'); ?>
+					<?php endwhile; ?>
+				</div>
+				<?php get_template_part('template-parts/content/pagination'); ?>
+			</main>
+
+			<aside class="col-md-3">
+				<?php dynamic_sidebar('sidebar-posts') ?>
+			</aside>
+		</div>
+	</div>
 </div>
+
 
 <?php get_footer(); ?>

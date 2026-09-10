57d1e0a5705a8e41907d88b76ef9033b6bce4d1b
Filipe de Souza <filipe.souza@hacklab.com.br>
2026-09-10 16:18:24 -0300

    ajustes no bloco donate #304

--- themes/midia-ninja-theme/dist/js/functionalities/embed-cpi-modal.asset.php
+++ themes/midia-ninja-theme/dist/js/functionalities/embed-cpi-modal.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '75988166864d730d2081dfd7ea927f62');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f0a0ebf466c67367f6bfea3b286e972a');
